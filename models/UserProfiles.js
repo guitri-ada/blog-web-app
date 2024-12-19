42129431d6a47cacc832a44a00a30bb864d462e6
asamac99 <asamac@hotmail.co.uk>
2024-12-19 15:27:42 +0000

    Add firstname and lastname fields to UserProfiles schema

--- models/UserProfiles.js
+++ models/UserProfiles.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userProfileSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     bio: { type: String },
 });
 
