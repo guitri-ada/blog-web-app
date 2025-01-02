3b42db5e0c41ad925571a069b643220034c3f4b6
asamac99 <asamac@hotmail.co.uk>
2025-01-02 15:37:24 +0000

    Add Create Profile button and update Delete Profile button styling in ProfileActions component

--- frontend/src/components/ProfileActions.jsx
+++ frontend/src/components/ProfileActions.jsx
@@ -6,14 +6,19 @@ import AddIcon from '@mui/icons-material/Add';
 
 const ProfileActions = ({ handleClick, setOpenDialog, setOpenCreateDialog }) => (
   <Box sx={{ width: '100%', maxWidth: '600px', display: 'flex', justifyContent: 'space-between', mb: 4 }}>
+    <Button variant="contained" color="success" onClick={() => setOpenCreateDialog(true)} startIcon={<AddIcon />}>
+      Create Profile
+    </Button>
     <Button variant="contained" color="primary" onClick={handleClick} startIcon={<EditIcon />}>
       Update Profile
     </Button>
-    <Button variant="contained" color="secondary" onClick={() => setOpenDialog(true)} startIcon={<DeleteIcon />}>
+    <Button
+      variant="contained"
+      sx={{  backgroundColor: '#8B0000'  }}
+      onClick={() => setOpenDialog(true)}
+      startIcon={<DeleteIcon />}
+    >
       Delete Profile
-    </Button>
-    <Button variant="contained" color="success" onClick={() => setOpenCreateDialog(true)} startIcon={<AddIcon />}>
-      Create Profile
     </Button>
   </Box>
 );
