97a0c93f83fe48d61d15e160e29dc1bbaa213056
Ross-T <104386690+Ross-T@users.noreply.github.com>
2025-01-22 14:06:56 +0000

    migrate from MongoClient to mongoose

--- server.js
+++ server.js
@@ -1,58 +1,40 @@
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 const ATLAS_URI = process.env.ATLAS_URI;
 
+// Connect to MongoDB using Mongoose
+mongoose.connect(ATLAS_URI)
+  .then(() => console.log('Connected to MongoDB Atlas!'))
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  });
+
 // Middleware
 app.use(express.json());
-
-let db;
-
-// Connect to MongoDB Atlas
-async function connectToDatabase() {
-    const client = new MongoClient(ATLAS_URI, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-
-    try {
-        await client.connect();
-        db = client.db('blog_app');
-        console.log("Connected to MongoDB Atlas!");
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-connectToDatabase().catch(console.dir);
+app.use(cookieParser());
 
 // Routes
-
-// > Blog post
 const blogPostRoutes = require('./routes/blogPostRoute');
 app.use('/api/blogPosts', blogPostRoutes);
 
-// > User
-const userProfilesRoute = require('./routes/userProfilesRoute');
-app.use('/api/userProfiles', (req, res, next) => {
-    req.db = db;
-    next();
-}, userProfilesRoute);
+const authRoute = require('./routes/authRoute');
+app.use('/api/auth', authRoute);
 
-// > etc...
+const userProfilesRoute = require('./routes/userProfilesRoute');
+app.use('/api/userProfiles', userProfilesRoute);
 
 // Root route for status check
 app.get('/', (req, res) => {
-    res.send('Server is running, and MongoDB is connected!');
+  res.send('Server is running, and MongoDB is connected!');
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
