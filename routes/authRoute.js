e8203548a97ad7e1be35c25cabf1e3812252d6d2
asamac99 <asamac@hotmail.co.uk>
2025-02-05 14:32:01 +0000

    add email field to new user profile creation and update profile existence check logic

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -45,7 +45,7 @@ router.post(
       await newUser.save();
 
       // Create an empty user profile
-      const newUserProfile = new UserProfiles({ user: newUser._id });
+      const newUserProfile = new UserProfiles({ user: newUser._id, email });
       await newUserProfile.save();
 
       res.status(201).json({ message: 'User registered successfully!' });
@@ -81,7 +81,7 @@ router.post(
 
       // Check if user has a profile
       const userProfile = await UserProfiles.findOne({ user: user._id });
-      const hasProfile = !!userProfile.firstname && !!userProfile.lastname && !!userProfile.bio;
+      const hasProfile = !userProfile.firstname && !userProfile.lastname && !userProfile.bio;
 
       // Generate token
       const token = jwt.sign({ id: user._id, username: user.username, hasProfile }, JWT_SECRET, { expiresIn: '1h' });
