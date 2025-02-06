d4c8baf2c399edf025a958925ef229e10f8b9762
asamac99 <asamac@hotmail.co.uk>
2025-02-06 11:30:52 +0000

    fix: standardize quotation marks and improve formatting in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const csrf = require('csurf');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 const csrfProtection = csrf({ cookie: true });
 const app = express();
 
@@ -10,10 +10,11 @@ const PORT = process.env.PORT || 5000;
 const ATLAS_URI = process.env.ATLAS_URI;
 
 // Connect to MongoDB using Mongoose
-mongoose.connect(ATLAS_URI)
-  .then(() => console.log('Connected to MongoDB Atlas!'))
+mongoose
+  .connect(ATLAS_URI)
+  .then(() => console.log("Connected to MongoDB Atlas!"))
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   });
 
@@ -23,24 +24,23 @@ app.use(cookieParser());
 app.use(csrfProtection);
 
 // Routes
-const blogPostRoutes = require('./routes/blogPostRoute');
-app.use('/api/blogPosts', blogPostRoutes);
+const blogPostRoutes = require("./routes/blogPostRoute");
+app.use("/api/blogPosts", blogPostRoutes);
 
-const authRoute = require('./routes/authRoute');
-app.use('/api/auth', authRoute);
+const authRoute = require("./routes/authRoute");
+app.use("/api/auth", authRoute);
 
-const userProfilesRoute = require('./routes/userProfilesRoute');
-app.use('/api/userProfiles', userProfilesRoute);
+const userProfilesRoute = require("./routes/userProfilesRoute");
+app.use("/api/userProfiles", userProfilesRoute);
 
 // Root route for status check
-app.get('/', (req, res) => {
-  res.send('Server is running, and MongoDB is connected!');
+app.get("/", (req, res) => {
+  res.send("Server is running, and MongoDB is connected!");
 });
 
-app.get('/api/csrf-token', (req, res) => {
+app.get("/api/csrf-token", (req, res) => {
   res.json({ csrfToken: req.csrfToken() });
 });
-
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
