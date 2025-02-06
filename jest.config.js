38da148cb8de861b99b083a9df5f8976af4ee25f
guitri-ada <guillaume.trionfo@ada.ac.uk>
2025-02-06 09:36:17 +0000

    tests update

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+  /** @returns {Promise<import('jest').Config>} */
+module.exports = async () => {
+    return {
+      verbose: true,
+      projects: [
+        {
+          displayName: "frontend",
+          testEnvironment: "jsdom",
+          testMatch: ["<rootDir>/frontend/**/*.{test.js,test.jsx}"],
+          setupFilesAfterEnv: ["<rootDir>/jest.config.js"]
+        },
+        {
+          displayName: "backend",
+          testEnvironment: "node",
+          testMatch: ["<rootDir>/tests/**/*.test.js"],
+          setupFilesAfterEnv: ["<rootDir>/tests/setup.js"]
+        }
+      ],
+      transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+      },
+      testEnvironment: "jsdom"
+    };
+  };
+  
