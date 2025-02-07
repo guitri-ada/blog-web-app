9b97fd0019dff8e348984f3ac9939eb4828a987b
Ross-T <ross_turpin@yahoo.co.uk>
2025-02-07 17:57:51 +0000

    add comments and documentation for clarity

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,23 +3,17 @@ import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 import AuthContext from "../contexts/AuthContext.jsx";
 
+// Redirects based on authentication and profile status.
 const ProtectedRoute = ({ children, requireProfile }) => {
   const { isAuthenticated, hasProfile } = useContext(AuthContext);
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" />;
-  }
-
-  if (requireProfile && !hasProfile) {
-    return <Navigate to="/create-profile" />;
-  }
-
-  if (!requireProfile && hasProfile) {
-    return <Navigate to="/" />;
-  }
+  if (!isAuthenticated) return <Navigate to="/login" />;
+  if (requireProfile && !hasProfile) return <Navigate to="/create-profile" />;
+  if (!requireProfile && hasProfile) return <Navigate to="/" />;
 
   return children;
 };
+
 ProtectedRoute.propTypes = {
   children: PropTypes.node.isRequired,
   requireProfile: PropTypes.bool.isRequired,
