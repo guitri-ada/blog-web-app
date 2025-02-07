dd4d5a7f6c76104f1f903a124f16b98f680cf8a2
Ross-T <ross_turpin@yahoo.co.uk>
2025-02-07 14:25:27 +0000

    remove duplicate Jest configuration file

--- frontend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["js", "jsx"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
-  setupFilesAfterEnv: ["<rootDir>/../tests/setup.js"],
-};
